2eabfa7d56ee1a62891785c914edec0499399879
Mathubala95 <madhuvalar.govindaraj@gmail.com>
2023-02-07 10:48:50 +0530

    now

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-    api : "http://localhost:3000",
-    
+    // api : "http://localhost:3000",
+    api : "https://paytm-backend.onrender.com",
     storage_key : "paytm_app"
 }
